a31e93608151cc1a44a37597bd3832e0bbce175a
Sanko Robinson <sanko@cpan.org>
2022-06-18 00:51:20 -0400

    ...

--- builder/Dyn.pm
+++ builder/Dyn.pm
@@ -67,8 +67,8 @@ sub alien {
             = ( $^O eq 'MSWin32' ? '.\configure.bat /prefix ' : './configure --prefix=' );
         warn($_) && system($_ )
             for grep {defined} $configure . $pre->absolute, # . ' CFLAGS="-Ofast" LDFLAGS="-Ofast"',
-            ( $^O eq 'MSWin32' ? 'cd ..' : () ), 'make V=1',
-            'make V=1 install';
+            ( $^O eq 'MSWin32' ? 'cd ..' : () ), 'make V=1 -f Makefile.generic',
+            'make V=1 -f Makefile.generic install';
         warn Path::Tiny->cwd->absolute;
         chdir $cwd->stringify;
     }
