ba1f8c79490daaf256def6c912e0614c464f5c6c
Sanko Robinson <sanko@cpan.org>
2023-02-02 11:41:38 -0500

    Checking in changes prior to tagging of version 0.10_06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1e8ecb7..96eb423 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,7 +2,7 @@ Revision history for Perl extension Affix.pm
     
     {{$NEXT}}
     
    -0.10_05 2023-02-02T15:30:46Z
    +0.10_06 2023-02-02T16:37:51Z
     
         - Unstable TRIAL release

--- lib/Affix.pm
+++ lib/Affix.pm
@@ -10,7 +10,7 @@ package Affix {    # 'FFI' is my middle name!
     use Text::ParseWords;
     use Carp qw[];
     use vars qw[@EXPORT_OK @EXPORT %EXPORT_TAGS];
-    our $VERSION = '0.10_05';
+    our $VERSION = '0.10_06';
     use XSLoader;
     XSLoader::load( __PACKAGE__, $VERSION );
     #
